87e1669bafab7c3a30bce3e25f03e3cbc50879e3
huunguyennguyen98 <116079984+huunguyennguyen98@users.noreply.github.com>
2023-04-08 12:40:52 +0700

    Initial commit

---
+++ app/src/main/java/fsbmse11hcm/iot/iotdashboard/MainActivity.java
@@ -0,0 +1,14 @@
+package fsbmse11hcm.iot.iotdashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
